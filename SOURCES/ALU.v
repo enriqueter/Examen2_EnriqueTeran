c1bd79c2f41bddc0e7ba3be197d84b136e021abc
Enrique <eteran9669@gmail.com>
2017-11-02 09:00:45 -0600

    cambios hechos

---
+++ SOURCES/ALU.v
@@ -0,0 +1,57 @@
+/******************************************************************
+* Description
+*	This is an 32-bit arithetic logic unit that can execute the next set of operations:
+*		add
+*		sub
+*		or
+*		and
+*		nor
+* This ALU is written by using behavioral description.
+* Version:
+*	1.0
+* Author:
+*	Dr. José Luis Pizano Escalante
+* email:
+*	luispizano@iteso.mx
+* Date:
+*	01/03/2014
+******************************************************************/
+
+module ALU 
+(
+	input [3:0] ALUOperation,
+	input [31:0] A,
+	input [31:0] B,
+	output reg Zero,
+	output reg [31:0]ALUResult
+);
+localparam AND = 4'b0000;
+localparam SQU  = 4'b0001;
+localparam NOR = 4'b0010;
+localparam ADD = 4'b0011;
+localparam SUB = 4'b0110;
+localparam MOV = 4'b1111;
+
+   
+   always @ (A or B or ALUOperation)
+     begin
+		case (ALUOperation)
+		  ADD: // add
+			ALUResult=A + B;
+		  SUB: // sub
+			ALUResult=A - B;
+		  AND: // and
+			ALUResult= A & B;
+		  SQU: // Square
+			ALUResult= A * A;
+		  NOR: // or
+			ALUResult= ~(A|B);
+		  MOV: // move
+		   ALUResult= A == B;
+			
+		default:
+			ALUResult= 0;
+		endcase // case(control)
+		Zero = (ALUResult==0) ? 1'b1 : 1'b0;
+     end // always @ (A or B or control)
+endmodule // ALU
